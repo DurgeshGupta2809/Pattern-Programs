edfec9a8d0a3ef29f074e119ae317b1e9464c18a
Durgesh Gupta <130821505+DurgeshGupta2809@users.noreply.github.com>
2024-04-11 17:50:55 +0530

    Add files via upload

---
+++ Arjun.java
@@ -0,0 +1,17 @@
+package Pattern;
+
+public class Arjun {
+
+	public static void main(String args[]) 
+	{
+	int r,c;
+		for(r=1; r<=6; r++)
+		{
+			for(c=1; c<=6; c++)
+			{
+				System.out.print("* ");
+			}
+		  System.out.println("  ");
+		}
+	}
+}
---
+++ Bheem.java
@@ -0,0 +1,18 @@
+package Pattern;
+
+public class Bheem {
+
+	public static void main(String[] args) {
+		
+		for(int i=1; i<=6; i++)
+		{
+			for(int j=1; j<=6; j++)
+			{
+				System.out.print(i+" ");
+			}
+		  System.out.println();
+		}
+
+	}
+
+}
